d999496f2b13894238bcbe70304e858e4ef9e70f
jsaussereau <jsaussereau@protonmail.com>
2022-04-10 16:59:52 +0200

    Séparation des fonctions par étapes de développement

--- src/lib_LCD.c
+++ src/lib_LCD.c
@@ -10,58 +10,102 @@
 
 #include "lib_LCD.h"
 
-//******************************************************************
-// Initialisation générale de l'afficheur en mode 4 bits
-//******************************************************************
-void lcd_init() {
+/************************************************************************
+                                ETAPE 1
+ Simplification des accès 
+************************************************************************/
+
+// voir dans "Header Files" => lib_LCD.h
+
+
+/************************************************************************
+                                ETAPE 2
+ Développement de fonctions d'envoi de n'importe quelle instruction
+************************************************************************/
+
+// Envoi d'une instruction au format 4 bits (initialisation) vers le module LCD
+void lcd_write_instr_4bits(unsigned char operation, unsigned char c){
     
 }
 
-//******************************************************************
+// Envoi d'une instruction au format 8 bits vers le module LCD
+void lcd_write_instr_8bits(unsigned char operation, unsigned char c) {
+    
+}
+
 // Test le bit BF jusqu'à ce que l'afficheur soit prêt
-//******************************************************************
 void lcd_busy() {
     
 }
 
-//******************************************************************
-// Envoi d'une instruction vers le module LCD
-//******************************************************************
-void lcd_write_instr(unsigned char c) {
-    
-}
 
-//******************************************************************
-// Ecriture d'un caractère sur l'afficheur
-//******************************************************************
-void lcd_putch(unsigned char c) {
-    
-}
+/************************************************************************
+                                ETAPE 3
+ Développement des fonctions correspondant aux différentes instructions
+************************************************************************/
 
-//******************************************************************
-// Ecriture d'une chaine de caractères sur l'afficheur
-//******************************************************************
-void lcd_puts(const unsigned char *s) {
-    
-}
+// voir DS_Afficheurs_Sunplus.pdf p5, p6 et p7
 
-//******************************************************************
-// Positionnement du curseur en (x,y) - origine en (1,1)
-//******************************************************************
-void lcd_pos(unsigned char ligne, unsigned char pos) {
-    
-}
-
-//******************************************************************
-// Effacement de l'ecran et cursor home
-//******************************************************************
+// Effacement de l'écran
 void lcd_clear() {
     
 }
 
-//******************************************************************
-// Cursor home
-//******************************************************************
+// Retour du curseur à l'origine
 void lcd_home() {
+    
+}
+
+// Direction du curseur et décalage pendant l'ecriture
+void lcd_entry_mode_set(unsigned char inc_dec, unsigned char shift) {
+    
+}
+
+// Ecran ON/OFF, Curseur ON/OFF, Clignotement ON/OFF
+void lcd_display_control(unsigned char display, unsigned char cursor, unsigned char blink) {
+    
+}
+
+// Déplacement du curseur
+void lcd_shift_cursor(signed char amount) {
+    
+}
+
+// Taille des données (4 / 8 bits), nombre de lignes, taille des caractères
+void lcd_function_set(unsigned char data_length, unsigned char lines, unsigned char font) {
+    
+}
+
+
+/************************************************************************
+                                ETAPE 4
+ Développement de la fonction d'initialisation
+************************************************************************/
+
+// voir DS_Afficheurs_Sunplus.pdf p10 et p11
+
+// Initialisation générale de l'afficheur
+void lcd_init() {
+    
+}
+
+
+/************************************************************************
+                                ETAPE 5
+ Développement des fonctions utilisateur restantes.
+************************************************************************/
+
+// Ecriture d'un caractère sur l'afficheur
+void lcd_putch(unsigned char c) {
+    
+}
+
+// Ecriture d'une chaine de caractères sur l'afficheur
+void lcd_puts(const unsigned char *s) {
+    
+}
+
+// Positionnement du curseur en (x,y) - origine en (1,1)
+void lcd_pos(unsigned char ligne, unsigned char pos) {
     
 }
--- src/lib_LCD.h
+++ src/lib_LCD.h
@@ -13,20 +13,32 @@
 
 #include <xc.h>  
 
-/*
-On introduira ici tous les symboles et macros utiles qui faciliteront 
-la maintenance et la lisibilité du fichier source correspondant
-*/
 
+// Etape 1 : Simplification des accès
+// On introduira ici tous les symboles et macros utiles qui faciliteront 
+// la maintenance et la lisibilité du fichier source correspondant
 #define RS RD4 // entrée RS reliée à RD4
+//...
 
-void lcd_init();
+// Etape 2 : Développement de fonctions d'envoi de n'importe quelle instruction
+void lcd_write_instr_4bits(unsigned char operation, unsigned char c);
+void lcd_write_instr_8bits(unsigned char operation, unsigned char c);
 void lcd_busy();
-void lcd_write_instr(unsigned char c);
+
+// Etape 3 : Développement des fonctions correspondant aux différentes instructions
+void lcd_home();
+void lcd_clear();
+void lcd_entry_mode_set(unsigned char inc_dec, unsigned char shift);
+void lcd_display_control(unsigned char display, unsigned char cursor, unsigned char blink);
+void lcd_shift_cursor(signed char amount);
+void lcd_function_set(unsigned char data_length, unsigned char lines, unsigned char font);
+
+// Etape 4 : Développement de la fonction d'initialisation
+void lcd_init();
+
+// Etape 5 : Développement des fonctions utilisateur restantes.
 void lcd_putch(unsigned char c);
 void lcd_puts(const unsigned char *s);
 void lcd_pos(unsigned char ligne, unsigned char pos);
-void lcd_clear();
-void lcd_home(void);
 
 #endif	/* __LIB_LCD */
